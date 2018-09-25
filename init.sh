b54f0bac6e4a304c1da59fa65f51dc2f8d793208
Martin Rhoads <ermal14@gmail.com>
2018-09-24 20:16:44 -0700

    add toolrc

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 set -o errexit
 set -o xtrace
 
 echo hi
 
+cd $HOME
+curl -o .toolrc https://raw.githubusercontent.com/martinrhoads/toolbox/master/toolrc
+echo 'source $HOME/.toolrc' >> .bashrc
+echo 'source $HOME/.toolrc' >> .zshrc
 
 echo bye
