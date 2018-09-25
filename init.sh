95d63898a04e8bd65a6929ba06afde0968d14483
Martin Rhoads <ermal14@gmail.com>
2018-09-24 20:56:05 -0700

    set brew completion message

--- init.sh
+++ init.sh
@@ -6,8 +6,12 @@ set -o xtrace
 echo hi
 
 cd $HOME
+
+if [ ! -f "$file" ]; then
+    echo 'source $HOME/.toolrc' >> .bashrc
+    echo 'source $HOME/.toolrc' >> .zshrc
+fi
+
 curl -o .toolrc https://raw.githubusercontent.com/martinrhoads/toolbox/master/toolrc
-echo 'source $HOME/.toolrc' >> .bashrc
-echo 'source $HOME/.toolrc' >> .zshrc
 
 echo bye
