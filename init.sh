2364e142f59cb018cdb7cc2ecf640595b621d9e4
Martin Rhoads <mrhoads@slack-corp.com>
2018-10-04 18:19:42 -0700

    clean init.sh

--- init.sh
+++ init.sh
@@ -3,15 +3,17 @@
 set -o errexit
 set -o xtrace
 
+toolSrc="https://raw.githubusercontent.com/martinrhoads/toolbox/master/toolrc"
 toolPath=$HOME/.toolrc
 
 if [ -f "$toolPath" ]; then
     echo tool already installed. \"source $toolPath\" and run \"updatetool\".
 else
     echo installing tool...
-    curl --fail --silent --output $toolPath\
-         https://raw.githubusercontent.com/martinrhoads/toolbox/master/toolrc
+    curl --fail --silent --output $toolPath $toolSrc
     echo "source $toolPath" >> .bashrc
     echo "source $toolPath" >> .zshrc
     sudo adduser martin docker
 fi
+
+echo tool installed.
